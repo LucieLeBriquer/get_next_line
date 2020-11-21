a84d3099dc01d24bf45867b8b2053ac840ba67b9
Lucie Le Briquer <contact@lucie-lebriquer.fr>
2020-11-21 14:22:25 +0100

    BUFF_0 test

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lle-briq <lle-briq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 17:54:41 by lle-briq          #+#    #+#             */
-/*   Updated: 2020/11/21 13:47:38 by lle-briq         ###   ########.fr       */
+/*   Updated: 2020/11/21 14:20:05 by lle-briq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,11 @@
 static int	check_errors(int fd, char **line, int size, int is_start)
 {
 	if (BUFFER_SIZE <= 0)
-		return (-1);
+	{
+		if (line)
+			*line = 0;
+		return (0);
+	}
 	if (is_start)
 		*line = malloc(sizeof(char));
 	if (!(*line))
