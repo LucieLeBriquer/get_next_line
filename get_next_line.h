8189e359b84266758467dd9aa84044de498f9fac
Lucie Le Briquer <contact@lucie-lebriquer.fr>
2020-11-21 15:22:22 +0100

    remove headers useless

--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,14 @@
 /*   By: lle-briq <lle-briq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 17:54:58 by lle-briq          #+#    #+#             */
-/*   Updated: 2020/11/21 14:45:31 by lle-briq         ###   ########.fr       */
+/*   Updated: 2020/11/21 15:21:13 by lle-briq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# include <stdio.h>
-# include <stdlib.h>
 # include <fcntl.h>
 # include <unistd.h>
-# include <string.h>
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 32
 # endif
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,17 +6,14 @@
 /*   By: lle-briq <lle-briq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 17:54:58 by lle-briq          #+#    #+#             */
-/*   Updated: 2020/11/21 14:42:54 by lle-briq         ###   ########.fr       */
+/*   Updated: 2020/11/21 15:21:50 by lle-briq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
-# include <stdio.h>
-# include <stdlib.h>
 # include <fcntl.h>
 # include <unistd.h>
-# include <string.h>
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 32
 # endif
